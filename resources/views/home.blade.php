f7525382ff33546a434302939a3bb32348c315bb
Pratap-Pawar <pratap.pawar@anyonehome.com>
2024-01-10 17:43:16 +0530

    project setup
    initial project setup with node and npm

---
+++ resources/views/home.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>My Blog</title>
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    </head>
+    <body>
+        <script src="{{ asset('js/app.js') }}"></script>
+    </body>
+</html>
